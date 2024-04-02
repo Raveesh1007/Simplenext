814909024bc0bc01a0086ecf230a5a88a85bba79
Raveesh Kumar <92170670+Raveesh1007@users.noreply.github.com>
2024-04-02 16:56:14 +0530

    1stcommit

---
+++ simple/app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import React from "react" 
+export default function ({children}: {
+    children: React.ReactNode 
+}) {
+    return <div> 
+         <div className = "border-b text-center font-bold">
+        30% off for the next 3 days
+    </div>
+    {children}
+   </div> 
+}
